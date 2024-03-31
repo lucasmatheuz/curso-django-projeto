ebdc8dee17e2337855db7e5120fea6958e038203
Lucas <lks-matheus@hotmail.com>
2024-03-30 22:36:01 -0300

    Adicionando Templates e Ajustando os Settings.

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import HttpResponse
+
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+def contato(request):
+    return HttpResponse('Contato')
+
+def sobre(request):
+    return HttpResponse('Sobre')
